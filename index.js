38619c5c08827aa72090836e82442d852f1f16ef
Tauhid hossen <tauhidhossen04@gmail.com>
2024-04-26 22:53:03 +0600

    env file added for Secure password and user

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
@@ -13,8 +14,8 @@ app.use(express.json());
 //kOFcFxNlikfIBlhp
 
 const uri =
-  "mongodb+srv://Tourist:kOFcFxNlikfIBlhp@cluster0.moefco9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.moefco9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
