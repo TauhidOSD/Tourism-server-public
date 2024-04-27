6036fca2eb4adda148500ce6aa66b47d557d4583
Tauhid hossen <tauhidhossen04@gmail.com>
2024-04-28 00:23:14 +0600

    Delete operation Successfully done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,8 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.moefco9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.moefco9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,21 +26,28 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const placeCollection =client.db('placeDB').collection('Place');
-   
-    app.get('/Place',async(req,res)=>{
-        const cursor = placeCollection.find();
-        const result =await cursor.toArray();
+    const placeCollection = client.db("placeDB").collection("Place");
+
+    app.get("/Place", async (req, res) => {
+      const cursor = placeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/Place", async (req, res) => {
+      const Place = req.body;
+      console.log(Place);
+      const result = await placeCollection.insertOne(Place);
+      res.send(result);
+    });
+
+
+    app.delete('/Place/:id',async(req,res)=>{
+        const id =req.params.id;
+        const query ={_id: new ObjectId(id)}
+        const result = await placeCollection.deleteOne(query);
         res.send(result);
     })
-
-    app.post('/Place',async(req,res)=>{
-        const Place=req.body;
-        console.log(Place);
-        const result =await placeCollection.insertOne(Place);
-        res.send(result);
-    })
-
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
