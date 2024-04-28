8f7b8f18894694d09662690d5f55009957237e75
Tauhid hossen <tauhidhossen04@gmail.com>
2024-04-28 09:48:19 +0600

    update page crated

--- index.js
+++ index.js
@@ -34,6 +34,14 @@ async function run() {
       res.send(result);
     });
 
+
+    app.get('/Place/:id',async(req,res)=>{
+        const id =req.params.id;
+        const query ={_id: new ObjectId(id)}
+        const result =await placeCollection.findOne(query);
+        res.send(result);
+    })
+
     app.post("/Place", async (req, res) => {
       const Place = req.body;
       console.log(Place);
